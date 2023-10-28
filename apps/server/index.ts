e9884c4525a7494b19245f4eacb0bcc54c6b61a3
Suryansh Anand <spirit@anandsur.com>
2023-10-28 18:30:43 +0530

    chore: setup express server with typescript.

---
+++ apps/server/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
